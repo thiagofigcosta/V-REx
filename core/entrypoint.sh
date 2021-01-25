40ef70f6036b9966a17a61cdca13024a2a903637
Computa Hacker Boy <thiagofigcosta@gmail.com>
2021-01-25 13:05:36 -0300

    fix omp stack size (causing leak)

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -2,7 +2,10 @@
 
 args="$@"
 
-gdb ./vrex/src/bin/vrex $args # TODO remove gdb
+ulimit -s unlimited
+export OMP_STACKSIZE=2G
+
+./vrex/src/bin/vrex $args
 
 # TODO remove profiling
 valgrind --leak-check=yes /vrex/src/bin/vrex
