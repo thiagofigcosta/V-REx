c33f80ee843d7e265b048c1f533ede2d8e52a79f
Thiago <thiagofigcosta@gmail.com>
2021-02-09 00:17:25 -0200

    allow single thread for every function

--- core/src/vrex/main.cpp
+++ core/src/vrex/main.cpp
@@ -19,7 +19,7 @@ bool trap_signals=false;
 bool connect_mongo=true;
 bool set_stack_size=false;
 bool cache_genetic=true;
-bool single_thread_for_genetic=false;
+bool single_thread=false;
 string custom_mongo_ip=""; 
 
 // Function used to check that 'opt1' and 'opt2' are not specified at the same time.
@@ -504,7 +504,7 @@ int main(int argc, char* argv[]) {
             ("eval-neural", boost::program_options::bool_switch()->default_value(false), "eval smart neural network")
             ("debug-args", boost::program_options::bool_switch()->default_value(false), "just print arguments (DEBUG)")
             ("do-not-cache-genetic", boost::program_options::bool_switch()->default_value(false), "avoid disk cache during genetic algorithm")
-            ("single-thread-genetic", boost::program_options::bool_switch()->default_value(false), "use a single thread for genetic algorithm")
+            ("single-thread", boost::program_options::bool_switch()->default_value(false), "use a single thread to run V-REx")
             ("test-function", boost::program_options::value<int>()->default_value(0), "specify the test function to run <function id>:\n\t1 - testCsvRead\n\t2 - testMongo\n\t3 - testSlide_IntLabel\n\t4 - testSlide_NeuronByNeuronLabel\n\t5 - testStdGeneticsOnMath\n\t6 - testEnchancedGeneticsOnMath\n\t7 - testSlide_Validation\n\t8 - testGeneticallyTunedNeuralNetwork\n\t9 - testMongoCveRead\n\t10 - testSmartNeuralNetwork_cveData\n\t11 - testGeneticallyTunedSmartNeuralNetwork_cveData")
             ("simulation-id", boost::program_options::value<string>()->default_value(""), "mongo genetic simulation id to fetch data <id>")
             ("network-id", boost::program_options::value<string>()->default_value(""), "mongo neural network id to fetch data <id>")
@@ -533,7 +533,7 @@ int main(int argc, char* argv[]) {
         trap_signals=vm["trap-signals"].as<bool>();
         connect_mongo=!vm["ignore-mongo"].as<bool>();
         cache_genetic=!vm["do-not-cache-genetic"].as<bool>();
-        single_thread_for_genetic=vm["single-thread-genetic"].as<bool>();
+        single_thread=vm["single-thread"].as<bool>();
         set_stack_size=vm["set-stack-size"].as<bool>();
         run_test=vm["test"].as<bool>();
         run_genetic=vm["run-genetic"].as<bool>();
@@ -561,7 +561,7 @@ int main(int argc, char* argv[]) {
         cout<<"connect_mongo: "<<connect_mongo<<endl;
         cout<<"set_stack_size: "<<set_stack_size<<endl;
         cout<<"cache_genetic: "<<cache_genetic<<endl;
-        cout<<"single_thread_for_genetic: "<<single_thread_for_genetic<<endl;
+        cout<<"single_thread: "<<single_thread<<endl;
         cout<<endl;
         cout<<"run_test: "<<run_test<<endl;
         cout<<"run_genetic: "<<run_genetic<<endl;
@@ -575,8 +575,8 @@ int main(int argc, char* argv[]) {
         cout<<"eval_data: "<<eval_data<<endl;
         return 0;
     }
-    if (run_genetic && single_thread_for_genetic){
-        Slide::MAX_THREADS=1; // Trying to reduce mem usage
+    if (single_thread){
+        Slide::MAX_THREADS=1;
     }
     setup();
     if (run_test){
