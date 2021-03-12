48072782a154948b4d31f40b8a0b3bb2058af98f
Thiago <thiagofigcosta@gmail.com>
2021-03-11 21:17:10 -0300

    limit K values

--- core/src/vrex/Slide.cpp
+++ core/src/vrex/Slide.cpp
@@ -33,6 +33,12 @@ Slide::Slide(int numLayer, int *sizesOfLayers, NodeType* layerTypes, int InputDi
         cross_validation=crossValidation;
         size_max_for_layer=maxLayerS;
 
+        for(int i=0;i<amount_layers;i++){
+            if (K[i]>layer_sizes[i]){  // ??? K cannot be higher than layer size?
+                K[i]=layer_sizes[i];
+            }
+        }
+
         cout<<toString();
 
         slide_network=new Network(layer_sizes, layer_types, amount_layers, batch_size, learning_rate, input_dim, OutputDim, K, L, range_pow, sparsity,mode,hash_function,use_adam,label_type,size_max_for_layer);
